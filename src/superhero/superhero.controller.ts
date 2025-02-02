9e68604365063fb038c9c18228acd9cbfcfe9cca
Szabsza <szaby3000@gmail.com>
2025-02-02 14:00:26 +0200

    adding comments

--- src/superhero/superhero.controller.ts
+++ src/superhero/superhero.controller.ts
@@ -14,13 +14,25 @@ import { SuperheroService } from './superhero.service';
 export class SuperheroController {
   constructor(private superheroService: SuperheroService) {}
 
+  /**
+   * Handle GET requests to the /superheroes endpoint.
+   * This will return a list of superheroes sorted by their humility score in ascending order.
+   * @returns An array of superheroes sorted by humility score.
+   */
   @Get()
   public findAllSortedByHumilityScore(): Superhero[] {
     return this.superheroService.findAllSortedByHumilityScore();
   }
 
+  /**
+   * Handle POST requests to the /superheroes endpoint.
+   * This endpoint will allow users to add a new superhero to the list.
+   * It uses a DTO (Data Transfer Object) to validate the incoming data.
+   * @param addSuperheroDto - The data transfer object containing superhero data (name, superpower, humility score).
+   * @returns The newly created superhero object.
+   */
   @Post()
-  @UsePipes(new ValidationPipe())
+  @UsePipes(new ValidationPipe()) // Use ValidationPipe to automatically validate incoming data
   public addSuperhero(@Body() addSuperheroDto: AddSuperheroDto): Superhero {
     return this.superheroService.create(addSuperheroDto);
   }
--- src/superhero/superhero.service.ts
+++ src/superhero/superhero.service.ts
@@ -5,13 +5,25 @@ import { Superhero } from './interface/superhero.interface';
 export class SuperheroService {
   private superheroes: Superhero[] = [];
 
+  /**
+   * Create a new superhero and add it to the in-memory list.
+   * @param superhero - The superhero object containing name, superpower and humility score.
+   * @returns The newly added superhero object.
+   */
   public create(superhero: Superhero): Superhero {
     this.superheroes.push(superhero);
 
     return superhero;
   }
 
+  /**
+   * Get a sorted list of superheroes based on their humility score in ascending order.
+   * The superheroes with lower humility scores will appear first in the list.
+   * @returns A sorted array of superheroes based on humility score.
+   */
   public findAllSortedByHumilityScore(): Superhero[] {
+    // Make a copy of the superheroes array to avoid modifying the original array in place.
+    // Sorting will be done on the copy to keep the original array intact.
     const sortedSuperheroes = [...this.superheroes].sort(
       (superheroA, superheroB) =>
         superheroA.humilityScore - superheroB.humilityScore,
